cf3c8bb3944529e826f7cc6d0807efb7602589de
jspann <99931644+jasc59@users.noreply.github.com>
2022-11-06 23:40:35 -0600

    Assignment 4 Updates

--- queries.sql
+++ queries.sql
@@ -1,5 +1,7 @@
 -- Part 1: Test it with SQL
-
+create schema techjobs;
+CREATE USER 'techjobs'@'localhost' IDENTIFIED BY 'techjobs';
+GRANT ALL PRIVILEGES ON techjobs.* TO 'techjobs'@'localhost';
 -- Part 2: Test it with SQL
 
 -- Part 3: Test it with SQL
