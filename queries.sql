48e9ef50dffa90eaaf8f29f92994ece306153026
jspann <99931644+jasc59@users.noreply.github.com>
2022-11-10 00:05:56 -0600

    Assignment 4 Updates

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@ create schema techjobs;
 CREATE USER 'techjobs'@'localhost' IDENTIFIED BY 'techjobs';
 GRANT ALL PRIVILEGES ON techjobs.* TO 'techjobs'@'localhost';
 -- Part 2: Test it with SQL
-
+SELECT name FROM employer WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
 
 -- Part 4: Test it with SQL
--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,19 +1,31 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
+    @Autowired
+    private EmployerRepository employerRepository;
 
+    @RequestMapping("")
+    public String index(Model model){
+        List<Employer> employers = Streamable.of(employerRepository.findAll()).toList();
 
+        return "index";
+    }
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
         model.addAttribute(new Employer());
@@ -28,13 +40,16 @@ public class EmployerController {
             return "employers/add";
         }
 
+        employerRepository.save(newEmployer);
+
         return "redirect:";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional optEmployer = employerRepository.findById(employerId);
+
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
